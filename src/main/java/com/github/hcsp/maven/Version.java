956a796312fd33bbfb88eb4069b0eb6ab50c77ee
TddSamaNoMercy <xymreasonxxs@163.com>
2019-10-03 06:03:30 +0800

    实现比较语义化版本

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import java.util.Comparator;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,52 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        int[] v1 = parseToIntArray(version1);
+        int[] v2 = parseToIntArray(version2);
+        if (v1[0] != v2[0]) {
+            return Integer.compare(v1[0], v2[0]);
+        } else if (v1[1] != v2[1]) {
+            return Integer.compare(v1[1], v2[1]);
+        } else if (v1[2] != v2[2]) {
+            return Integer.compare(v1[2], v2[2]);
+        }
+        return 0;
+
+    }
+
+    public static int[] parseToIntArray(String s) {
+        int[] intArray = {0, 0, 0};
+        int flag1 = -1, flag2 = -1;//用来记录小数点位置
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '.') {
+                if (flag1 == -1) {
+                    flag1 = i;
+                    continue;
+                }
+                flag2 = i;
+            }
+        }
+        if (flag1 == -1) {
+            intArray[0] = Integer.parseInt(s);
+            return intArray;
+        } else if (flag2 == -1) {
+            intArray[0] = indexToInt(0, flag1, s);
+            intArray[1] = indexToInt(flag1 +1, s.length(), s);
+            return intArray;
+        } else {
+            intArray[0] = indexToInt(0, flag1, s);
+            intArray[1] = indexToInt(flag1+1, flag2, s);
+            intArray[2] = indexToInt(flag2+1, s.length(), s);
+            return intArray;
+        }
+    }
+
+    public static int indexToInt(int a, int b, String s) {
+        if (a - b != 0) {
+            return Integer.parseInt(s.substring(a, b));
+        }
+        return 0;
+    }
+
 }
