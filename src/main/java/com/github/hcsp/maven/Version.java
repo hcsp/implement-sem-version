77143e280929c80947ccb78cb75bf86fc5d84dbd
Zegendary <zxw1992513@qq.com>
2019-10-07 23:31:00 +0800

    compare version

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,7 +1,10 @@
 package com.github.hcsp.maven;
 
+import org.apache.maven.artifact.versioning.ComparableVersion;
+
 public class Version {
-    /**
+
+  /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
      *
      * <p>传入两个形如x.y.z的字符串，比较两个语义化版本的大小。如果version1小于version2，返回-1；如果version1大于
@@ -13,5 +16,9 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2){
+      ComparableVersion v1 = new ComparableVersion(version1);
+      ComparableVersion v2 = new ComparableVersion(version2);
+      return v1.compareTo(v2);
+    }
 }
