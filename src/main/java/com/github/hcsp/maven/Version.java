426e9933b9078cabd760d3d8a872ce9f48fb8941
JiaXinWang61 <55199085+JiaXinWang61@users.noreply.github.com>
2019-10-08 11:05:37 +0800

    还是借鉴了其他同学的 (#50)
    还是借鉴了其他同学的 (#50)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -3,15 +3,35 @@ package com.github.hcsp.maven;
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
-     *
+     * <p>
      * <p>传入两个形如x.y.z的字符串，比较两个语义化版本的大小。如果version1小于version2，返回-1；如果version1大于
      * version2，返回1。如果二者相等，返回0。
-     *
+     * <p>
      * <p>注意，如果传入的字符串形如x，则其等价于x.0.0。 如果传入的字符串形如x.y，则其等价于x.y.0。
      *
      * @param version1 传入的版本字符串1，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        int[] v1 = toArr(version1.split("\\."));
+        int[] v2 = toArr(version2.split("\\."));
+        for (int i = 0; i < v1.length; i++) {
+            if (v1[i] < v2[i]) {
+                return -1;
+            } else if (v1[i] > v2[i]) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public static int[] toArr(String[] strings) {
+        int[] a = {0, 0, 0};
+        for (int i = 0; i < strings.length; i++) {
+            a[i] = Integer.parseInt(strings[i]);
+        }
+        return a;
+
+    }
 }
