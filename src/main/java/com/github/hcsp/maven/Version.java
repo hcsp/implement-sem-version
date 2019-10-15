ce975463b1ddeb67af13b56d8faaf0dc97dcc6a1
42 <LiYingjie@protonmail.com>
2019-10-15 20:18:58 +0800

    feat: compare version number (#56)
    feat: compare version number (#56)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,40 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+
+        if (version1.length() == 0 || version2.length() == 0) {
+            return 0;
+        }
+
+        int i = 0, j = 0;
+        int n1, n2;
+        while (i < version1.length() || j < version2.length()) {
+            n1 = 0;
+            n2 = 0;
+
+            while (i < version1.length() && version1.charAt(i) != '.') {
+                n1 = n1 * 10 + (version1.charAt(i) - '0');
+                i++;
+            }
+
+            while (j < version2.length() && version2.charAt(j) != '.') {
+                n2 = n2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+            if (n1 > n2) {
+                return 1;
+            } else if (n1 < n2) {
+                return -1;
+            }
+
+            if (i < version1.length() && version1.charAt(i) == '.') {
+                ++i;
+            }
+            if (j < version2.length() && version2.charAt(j) == '.') {
+                ++j;
+            }
+        }
+        return 0;
+    }
 }
