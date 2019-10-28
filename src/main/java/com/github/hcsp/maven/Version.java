2aad9de4c0dd94c8a3dfdde104ef0617c25c056f
yuanxufeng <yuanxufeng@114study.com>
2019-10-28 17:50:15 +0800

    compare_version

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.maven;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +16,28 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        ArrayList<String> version1Arr=new ArrayList<>(Arrays.asList(version1.split("\\.")));
+        ArrayList<String> version2Arr=new ArrayList<>(Arrays.asList(version2.split("\\.")));
+        solveVersionString(version1Arr);
+        solveVersionString(version2Arr);
+        for(int i=0;i<3;i++){
+            int version1Num=Integer.parseInt(version1Arr.get(i));
+            int version2Num=Integer.parseInt(version2Arr.get(i));
+            int result=Integer.compare(version1Num,version2Num);
+            if(result!=0){
+                return result;
+            }
+        }
+        return 0;
+    }
+    private static void solveVersionString(ArrayList versionArr){
+        int versionLength=3;
+        int versionArrSize=versionArr.size();
+        if(versionArrSize<versionLength){
+            for(int i=0;i<versionLength-versionArrSize;i++){
+                versionArr.add("0");
+            }
+        }
+    }
 }
