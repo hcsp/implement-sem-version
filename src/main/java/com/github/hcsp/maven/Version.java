26b979efdc1a48f82c542a6a40298cf30fbd748e
jianglimin <jianglimin999@gmail.com>
2019-11-13 13:37:56 +0800

    实现语义化版本

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,52 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        version1 = convertVersionCompleted(version1);
+        version2 = convertVersionCompleted(version2);
+        int[] version1Arr = strArrToIntArr(StringUtils.split(version1, '.'));
+        int[] version2Arr = strArrToIntArr(StringUtils.split(version2, '.'));
+        int result = 0;
+        for (int i = 0; i < version1Arr.length; i++) {
+            if (version1Arr[i] > version2Arr[i]) {
+                result = 1;
+                break;
+            }
+            if (version1Arr[i] < version2Arr[i]) {
+                result = -1;
+                break;
+            }
+        }
+        return result;
+    }
+
+    // 将传入的版本号转换成完整的格式
+    private static String convertVersionCompleted(String version) {
+        int numOfPoint = StringUtils.countMatches(version, '.');
+        String resultVersion = null;
+        switch (numOfPoint) {
+            case 0:
+                resultVersion = version + ".0.0";
+                break;
+            case 1:
+                resultVersion = version + ".0";
+                break;
+            default:
+                resultVersion = version;
+        }
+        return resultVersion;
+    }
+
+    // 将字符串数组转换成int数组
+    private static int[] strArrToIntArr(String[] strs) {
+        int[] resultArr = new int[strs.length];
+        for (int i = 0; i < strs.length; i++) {
+            resultArr[i] = Integer.valueOf(strs[i]);
+        }
+        return resultArr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compare("1.1", "1.1.1"));
+    }
 }
