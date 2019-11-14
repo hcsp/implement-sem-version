bcd8225527de19e91b1bbade6d73b331915cac12
xuanzebin <38971117+xuanzebin@users.noreply.github.com>
2019-11-14 23:18:04 +0800

    feat: implement sem version (#65)
    feat: implement sem version (#65)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,22 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        String[] version1Array = version1.split("[.]");
+        String[] version2Array = version2.split("[.]");
+        for (int i = 0; true; i++) {
+            int intVersion1, intVersion2;
+            intVersion1 = version1Array.length - 1 < i ? 0 : Integer.parseInt(version1Array[i]);
+            intVersion2 = version2Array.length - 1 < i ? 0 : Integer.parseInt(version2Array[i]);
+            if (intVersion1 < intVersion2) {
+                return -1;
+            }
+            if (intVersion1 > intVersion2) {
+                return 1;
+            }
+            if (i == 2) {
+                return 0;
+            }
+        }
+    }
 }
