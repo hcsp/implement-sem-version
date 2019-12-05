ef216d82a047aa60c3f09933e05ee02a95cd5116
wgd <872440899@qq.com>
2019-12-05 13:32:30 +0800

    比较版本 (#69)
    比较版本 (#69)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.maven;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +17,30 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        //split里面要写正则的表达式
+        List<String> ver1List = new ArrayList<String>(Arrays.asList(version1.split("\\.")));
+        List<String> ver2List = new ArrayList<String>(Arrays.asList(version2.split("\\.")));
+
+        fixZero(ver1List);
+        fixZero(ver2List);
+
+        for (int i=0; i<ver1List.size(); i++) {
+            int ver1 = Integer.parseInt(ver1List.get(i));
+            int ver2 = Integer.parseInt(ver2List.get(i));
+            if ( ver1 > ver2 ) {
+                return 1;
+            }else if ( ver1 < ver2 ) {
+                return -1;
+            }
+        }
+
+        return 0;
+    }
+
+    private static void fixZero(List<String> list){
+        while (list.size()<3){
+            list.add("0");
+        }
+    }
 }
