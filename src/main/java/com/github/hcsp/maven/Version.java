fd4e8f2b3b6e7c7a6fd9a65982132791381963b3
qfdk <qfdk2010@gmail.com>
2019-12-22 16:18:04 +0100

    [Dev] Test compare two versions

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.maven;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +16,28 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+
+        List<String> array1 = Arrays.asList(version1.split("[.]"));
+        List<String> array2 = Arrays.asList(version2.split("[.]"));
+
+        if (array1.equals(array2)) {
+            return 0;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (Integer.parseInt(array1.get(i)) < Integer.parseInt(array2.get(i))) {
+                return -1;
+            } else if (Integer.parseInt(array1.get(i)) > Integer.parseInt(array2.get(i))) {
+                return 1;
+            }
+        }
+        // 默认相等
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(compare("1.100.0", "1.100.0"));
+    }
 }
