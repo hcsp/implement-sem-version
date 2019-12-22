08ff37244517b5034fcd757205ca2dd39befcd16
qfdk <qfdk@users.noreply.github.com>
2019-12-22 16:33:55 +0100

    [Dev] Test compare two versions (#74)
    [Dev] Test compare two versions (#74)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.maven;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +17,37 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+
+        List<String> array1 = new ArrayList(Arrays.asList(version1.split("[.]")));
+        List<String> array2 = new ArrayList(Arrays.asList(version2.split("[.]")));
+
+        complementArray(array1);
+        complementArray(array2);
+
+        if (array1.equals(array2)) {
+            return 0;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (Integer.parseInt(array1.get(i)) < Integer.parseInt(array2.get(i))) {
+                return -1;
+            } else if (Integer.parseInt(array1.get(i)) > Integer.parseInt(array2.get(i))) {
+                return 1;
+            }
+        }
+        // 默认相等
+        return 0;
+    }
+
+
+    static void complementArray(List<String> array) {
+        for (int i = array.size(); i < 3; i++) {
+            array.add("0");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compare("1.100", "1.100.0"));
+    }
 }
