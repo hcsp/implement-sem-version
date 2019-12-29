615ede2f8535f6393ad9f1de35466ef3112d3204
Xi Pu <xp19@duke.edu>
2019-12-28 16:24:38 -0800

    implement Version.compare

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import java.util.Arrays;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,30 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        if (version1.equals(version2)) {
+            return 0;
+        }
+
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        for (int i = 0; i < Math.max(v1.length, v2.length); i++) {
+            int one = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int two = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+
+            if(one < two){
+                return -1;
+            }
+            else if(one > two){
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString("1".split("\\.")));
+    }
 }
