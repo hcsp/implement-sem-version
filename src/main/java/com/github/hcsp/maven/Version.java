34ee194049152a458fd5d5cd36ecc37a1a62c496
Tan DuZhi <lucky.tanduzhi@gmail.com>
2020-01-07 02:10:37 +0800

    面向谷歌编程… 复制过来打开调试器理解一下

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.maven;
 
-public class Version {
+public class Version implements Comparable<Version> {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
      *
@@ -13,5 +13,31 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public final int[] numbers;
+
+    public Version(String version) {
+        final String split[] = version.split("\\-")[0].split("\\.");
+        numbers = new int[split.length];
+        for (int i = 0; i < split.length; i++) {
+            numbers[i] = Integer.valueOf(split[i]);
+        }
+    }
+
+    @Override
+    public int compareTo(Version another) {
+        final int maxLength = Math.max(numbers.length, another.numbers.length);
+        for (int i = 0; i < maxLength; i++) {
+            final int left = i < numbers.length ? numbers[i] : 0;
+            final int right = i < another.numbers.length ? another.numbers[i] : 0;
+            if (left != right) {
+                return left < right ? -1 : 1;
+            }
+        }
+        return 0;
+    }
+
+    public static int compare(String version1, String version2) {
+        return new Version(version1).compareTo(new Version(version2));
+    }
+
 }
