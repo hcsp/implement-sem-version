be4330dfa7c40a16a93c1afa7ec8453ccbffbce9
xdc <1434412784@qq.com>
2020-01-11 23:21:46 +0800

    初步的草稿

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,34 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        /*
+            要点：
+                1. 需要准确的“分割”出每个版本号
+                2. 要处理好版本号长度不一致的情况，如 x 与 x.y 等情况
+                3. 转换成 int 进行比较（容易实现）
+         */
+        String[] versions1 = version1.split("\\.");
+        String[] versions2 = version2.split("\\.");
+
+
+        for (int i = 0; i < 3; i++) {
+            int a = 0, b = 0;
+
+            if (i < versions1.length) {
+                a = Integer.parseInt(versions1[i]);
+            }
+            if (i < versions2.length) {
+                b = Integer.parseInt(versions2[i]);
+            }
+
+            if (a > b) {
+                return 1;
+            } else if (a < b) {
+                return -1;
+            }
+        }
+
+        return 0;
+    }
 }
