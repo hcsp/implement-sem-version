9b97ce755e3fa5ae33b61b7b39b718fbaf38bc3a
TangsongyuanmingQingHAD <had13625428701@aliyun.com>
2020-01-17 09:50:26 +0800

    语义化版本

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.maven;
 
-import com.sun.deploy.util.StringUtils;
+import java.util.regex.Pattern;
 
 public class Version {
     /**
@@ -16,18 +16,69 @@ public class Version {
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
     public static int compare(String version1, String version2) {
-        String[] v1Arr = version1.split("\\.");
-        String[] v2Arr = version2.split("\\.");
-        int maxLength = Math.max(v1Arr.length,v2Arr.length);
-        for(int i = 0;i < maxLength; i++) {
-           int v1 =  i < v1Arr.length ? Integer.valueOf(v1Arr[i]):0;
-           int v2 =  i < v2Arr.length ? Integer.valueOf(v2Arr[i]):0;
-           if(v1 != v2) {
-               return v1 > v2 ? 1 : -1;
-           }
+        int[] ver1Arr = verStr2IntArr(version1);
+        int[] ver2Arr = verStr2IntArr(version2);
+
+        for (int i = 0; ; i++) {
+            int ver1 = ver1Arr[i];
+            int ver2 = ver2Arr[i];
+            if (ver1 == ver2 && i != 2) {
+                continue;
+            }
+            return Integer.compare(ver1, ver2);
         }
-        return  0;
     }
 
 
+    private static final String SPILT_REGEX = "\\.";
+
+    /**
+     * <p>
+     * 语义化版本字符串转 int 数组方法。
+     * 以 <code>.</code> 分割字符串成数组，数组长度恒为 <strong>3</strong>。
+     * 结果数组内的非<strong>正整数</strong>字符串将会转换为数字 <strong>0</strong>。
+     * </p>
+     * @param version 语义化版本字符串
+     * @return 转换结果字符串
+     */
+    private static int[] verStr2IntArr(String version) {
+        // 预定义结果数组
+        int[] result = {0, 0, 0};
+        // 字符串转数组
+        if (!isEmpty(version)) { // 字符串判空
+            String[] split = version.split(SPILT_REGEX);
+            if (split.length <= 0) {
+                return result;
+            }
+            int len = Math.min(result.length, split.length);
+            for (int i = 0; i < len; i++) {
+                result[i] = string2int(split[i]);
+            }
+        }
+        return result;
+    }
+
+    private static boolean isEmpty(String s) {
+        return s == null || s.length() < 1;
+    }
+
+    private static int string2int(String s) {
+        if (isPositiveInt(s)) {
+            return Integer.parseInt(s);
+        }
+        return 0;
+    }
+
+
+    private static boolean isPositiveInt(String s) {
+        if (isEmpty(s)) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile("[0-9]*");
+        return pattern.matcher(s).matches();
+    }
+
 }
+
+
+
