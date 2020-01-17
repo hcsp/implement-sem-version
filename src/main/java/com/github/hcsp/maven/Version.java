8f1d6a0004f8419938fb45db87048e554ba41f20
TangsongyuanmingQingHAD <had13625428701@aliyun.com>
2020-01-17 09:15:22 +0800

    maven语义化

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import com.sun.deploy.util.StringUtils;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,19 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        String[] v1Arr = version1.split("\\.");
+        String[] v2Arr = version2.split("\\.");
+        int maxLength = Math.max(v1Arr.length,v2Arr.length);
+        for(int i = 0;i < maxLength; i++) {
+           int v1 =  i < v1Arr.length ? Integer.valueOf(v1Arr[i]):0;
+           int v2 =  i < v2Arr.length ? Integer.valueOf(v2Arr[i]):0;
+           if(v1 != v2) {
+               return v1 > v2 ? 1 : -1;
+           }
+        }
+        return  0;
+    }
+
+
 }
