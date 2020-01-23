bb2e62fee5132b39c10ea9f64a0a298a759e02b7
Felix <fiymi666@gmail.com>
2020-01-24 07:25:57 +0800

    重新写方法

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,33 +1,6 @@
 package com.github.hcsp.maven;
 
 public class Version {
-
-
-    public static int[] drawValue(String version) {
-        int x = 0;
-        int y = 0;
-        int z = 0;
-        int[] list;
-        if (version.length() == 1) {
-            x = Integer.parseInt(version);
-            y = 0;
-            z = 0;
-        } else if (version.length() == 3) {
-            x = (int) version.charAt(0);
-            y = (int) version.charAt(2);
-            z = 0;
-        } else if (version.length() == 5) {
-            x = (int) version.charAt(0);
-            y = (int) version.charAt(2);
-            z = (int) version.charAt(4);
-        } else {
-            System.out.println("输入不符合规范");
-            System.exit(0);
-        }
-        list = new int[]{x, y, z};
-        return list;
-    }
-
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
      *
@@ -41,25 +14,28 @@ public class Version {
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
     public static int compare(String version1, String version2) {
-        int[] list1;
-        int[] list2;
-        int result = 0;
-        list1 = drawValue(version1);
-        list2 = drawValue(version2);
-        if (list1.equals(list2)) {
-            result = 0;
-        } else {
-            for (int i = 0; i > 2; ++i) {
-                if (list1[i] > list2[i]) {
-                    result = 1;
-                    break;
-                } else if (list1[i] < list2[i]) {
-                    result = -1;
-                    break;
-                } else {
-                    continue;
-                }
+        String[] lists1;
+        String[] lists2;
+        int result=0;
+        lists1=version1.split("\\.");
+        lists2=version2.split("\\.");
+        int length=Math.max(lists1.length,lists2.length);
+        for(int i=0;i>length;++i){
+            int list1=i<lists1.length?Integer.parseInt(lists1[i]):0;
+            int list2=i<lists2.length?Integer.parseInt(lists2[i]):0;
+            if(lists1.equals(lists2)){
+                result=0;
+                break;
             }
+            else if(list1>list2){
+                result=1;
+                break;
+            }
+            else if(list1<list2){
+                result=-1;
+                break;
+            }
+            else{continue;}
         }
         return result;
     }
