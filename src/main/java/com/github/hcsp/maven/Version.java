8ba20eb445e9993617fb6bbf7acb10b470462c88
Felix Mi <41504852+mm123mm123@users.noreply.github.com>
2020-01-24 10:52:51 +0800

    重新下载文件后，再次提交 (#96)
    重新下载文件后，再次提交 (#96)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,33 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+//    public static void main(String[] args){
+//        System.out.println(compare("1.1.1","1.1.0"));
+//    }
+    public static int compare(String version1, String version2) {
+        String[] lists1;
+        String[] lists2;
+        int result = 0;
+        lists1 = version1.split("\\.");
+        lists2 = version2.split("\\.");
+        int length = Math.max(lists1.length, lists2.length);
+        for (int i = 0; i <= length; ++i) {
+            int list1 = i < lists1.length ? Integer.parseInt(lists1[i]) : 0;
+            int list2 = i < lists2.length ? Integer.parseInt(lists2[i]) : 0;
+            if (lists1.equals(lists2)) {
+                result = 0;
+                break;
+            } else if (list1 > list2) {
+                result = 1;
+                break;
+            } else if (list1 < list2) {
+                result = -1;
+                break;
+            } else {
+                continue;
+            }
+        }
+        return result;
+    }
+
 }
