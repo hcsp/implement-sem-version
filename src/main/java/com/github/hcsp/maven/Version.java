8687cf2406875067a0ca667d6c46fd6abbefc4f3
mysteryven <33973865+mysteryven@users.noreply.github.com>
2020-02-01 15:02:26 +0800

    计算版本号的包 (#100)
    计算版本号的包 (#100)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,28 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        int[] versionList1 = preProduce(version1);
+        int[] versionList2 = preProduce(version2);
+
+        for (int i = 0; i < 3; i++) {
+            if (versionList1[i] > versionList2[i]) {
+                return 1;
+            } else if (versionList1[i] < versionList2[i]) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    public static int[] preProduce(String version) {
+        String[] numbers = version.split("\\.");
+        int[] res = new int[]{0, 0, 0};
+
+        for (int i = 0; i < numbers.length; i++) {
+            res[i] = Integer.parseInt(numbers[i]);
+        }
+
+        return res;
+    }
 }
