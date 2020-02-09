977afad4ebef9480d9bcf3318d027696c207f5d1
YingHuaLiu <1042413535@qq.com>
2020-02-09 21:36:01 +0800

    实现语义化版本

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import java.sql.SQLOutput;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,27 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        int[] versionList1 = preProduce(version1);
+        int[] versionList2 = preProduce(version2);
+
+        for (int i = 0; i < 3; i++) {
+            if (versionList1[i] > versionList2[i]) {
+                return 1;
+            } else if (versionList1[i] < versionList2[i]) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+   public static int[] preProduce(String version) {
+        String[] numbers = version.split("\\.");
+        int[] res = new int[]{0, 0, 0};
+
+        for (int i = 0; i < numbers.length; i++) {
+            res[i] = Integer.parseInt(numbers[i]);
+        }
+        return res;
+    }
 }
