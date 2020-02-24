3db0d9b45dfc8886fe3e912999cbe0c289d72f54
maxiaoda <631677979@qq.com>
2020-02-25 01:31:03 +0800

    Maven：实现语义化版本 //参考了最佳答案。

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,22 +13,17 @@ public class Version {
 //     * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
 //     * @return -1/0/1 当version1 小于/等于/大于 version2时
 //     **/
-
     public static int compare(String version1, String version2) {
-        if (version1.equals(version2)) {
-            return 0;
-        }
-        String[] version1Array = version1.split("\\.");
-        String[] version2Array = version2.split("\\.");
+        String[] v1Array = version1.split("\\.");
+        String[] v2Array = version2.split("\\.");
+        int max = Math.max(v1Array.length, v2Array.length);
 
-        int min = Math.min(version1Array.length, version2Array.length);
-        for (int i = 0; i < min; i++) {
-            if (Integer.parseInt(version1Array[i]) < Integer.parseInt(version2Array[i])) {
-                return -1;
-            } else if (Integer.parseInt(version1Array[i]) > Integer.parseInt(version2Array[i])) {
-                return 1;
+        for (int i = 0; i < max; i++) {
+            int v1 = (i < v1Array.length) ? Integer.parseInt(v1Array[i]) : 0;
+            int v2 = (i < v2Array.length) ? Integer.parseInt(v2Array[i]) : 0;
+            if (v1 != v2) {
+                return v1 > v2 ? 1 : -1;
             }
-
         }
         return 0;
     }
