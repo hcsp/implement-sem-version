6df94c8cdca735fa1d783c226515145d2082f825
breeze <1023349760@qq.com>
2020-03-07 00:53:23 +0800

    完成语义化版本比较

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import com.sun.deploy.security.SelectableSecurityManager;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,29 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        //分割字符串
+        String[] arr1 = version1.split("\\.");
+        String[] arr2 = version2.split("\\.");
+        //取分割数多的
+        int length = Math.max(arr1.length, arr2.length);
+        //进行循环比较，高到低只要有一位高则整体高
+        for (int i = 0; i < length; i++) {
+            int one = 0, two = 0;
+
+            if (arr1.length > i) {
+                one = Integer.parseInt(arr1[i]);
+            }
+            if (arr2.length > i) {
+                two = Integer.parseInt(arr2[i]);
+            }
+
+            if (one > two) {
+                return 1;
+            } else if (one < two) {
+                return -1;
+            }
+        }
+        return 0;
+    }
 }
