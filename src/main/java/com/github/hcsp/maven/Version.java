0859116eb53d39578d16b8720f9c360083a99446
tprox <61723644+tprox@users.noreply.github.com>
2020-03-14 02:22:19 +0800

    Implement compare method (#128)
    Implement compare method (#128)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,22 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        for (int i = 0; i < 3; i++) {
+            int n1 = v1.length > i ? Integer.parseInt(v1[i]) : 0;
+            int n2 = v2.length > i ? Integer.parseInt(v2[i]) : 0;
+            if (n1 < n2) {
+                return -1;
+            } else if (n1 > n2) {
+                return 1;
+            } else {
+                continue;
+            }
+        }
+
+        return 0;
+    }
 }
