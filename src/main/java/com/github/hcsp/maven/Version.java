47c6ea0156076694b70c8131fc9fdf5205c892a5
Tom Haiyi Wang <tomgoprox@gmail.com>
2020-03-14 02:30:52 +0800

    Implement compare method(refactor)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -14,18 +14,17 @@ public class Version {
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
     public static int compare(String version1, String version2) {
+        final int MAX_LEN = 3;
+
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < MAX_LEN; i++) {
             int n1 = v1.length > i ? Integer.parseInt(v1[i]) : 0;
             int n2 = v2.length > i ? Integer.parseInt(v2[i]) : 0;
-            if (n1 < n2) {
-                return -1;
-            } else if (n1 > n2) {
-                return 1;
-            } else {
-                continue;
+
+            if (n1 != n2) {
+                return n1 < n2 ? -1 : 1;
             }
         }
 
