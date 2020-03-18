807394dfba7cbe4611cbdc806b7d3f45656ce11d
DobbyKim <1175088275@qq.com>
2020-03-19 00:37:51 +0800

    add code

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,42 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        String[] version1Strs = version1.split(".");
+        String[] version2Strs = version2.split(".");
+        Integer[] help1 = new Integer[version1Strs.length + 2];
+        Integer[] help2 = new Integer[version2Strs.length + 2];
+        for(int i = 0;i < help1.length;i++){
+            if(i < version1Strs.length){
+                help1[i] = Integer.parseInt(version1Strs[i]);
+            }else{
+                help1[i] = 0;
+            }
+        }
+        for(int i = 0;i < help2.length;i++){
+            if(i < version2Strs.length){
+                help2[i] = Integer.parseInt(version2Strs[i]);
+            }else{
+                help2[i] = 0;
+            }
+        }
+        for(int i = 0;i < 3;i++){
+            if(i == 2 && help1[i] == help2[i]){
+                return 0;
+            }
+            if(i == 2 && help1[i] > help2[i]){
+                return 1;
+            }
+            if(i == 2 && help1[i] < help2[i]){
+                return -1;
+            }
+            if(help1[i] > help2[i]){
+                return 1;
+            }
+            if(help1[i] < help2[i]){
+                return -1;
+            }
+        }
+        return 0;
+    }
 }
