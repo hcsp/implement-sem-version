45d432ead704f4be22e82bc9106cd85184cbf4ad
Your Name <you@example.com>
2020-03-21 19:43:59 +0800

    fix

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -18,10 +18,6 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static void main(String[] args) {
-        System.out.println(compare("1.2.1", "1.2"));
-    }
-
     public static int compare(String version1, String version2) {
         for (int i = 0; i < 3; i++) {
             int v1 = Integer.parseInt(completeVersionInfo(version1).get(i));
