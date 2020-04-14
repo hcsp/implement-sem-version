a0961ee388e032966bef1dee7270f1d179824407
Song <36783363+JoeyPine@users.noreply.github.com>
2020-04-14 21:42:06 +0800

    Joey pine branch (#138)
    Joey pine branch (#138)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,18 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        String[] arr1 = version1.split("\\.");
+        String[] arr2 = version2.split("\\.");
+        int maxLength = Math.max(arr1.length, arr2.length);
+        for (int i = 0; i < maxLength; i++) {
+            int v1 = i < arr1.length ? Integer.parseInt(arr1[i]) : 0;
+            int v2 = i < arr2.length ? Integer.parseInt(arr2[i]) : 0;
+
+            if (v1 != v2) {
+                return v1 > v2 ? 1 : -1;
+            }
+        }
+        return 0;
+    }
 }
