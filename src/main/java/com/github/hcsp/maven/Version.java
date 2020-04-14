d1da7188fa0a8a666b4edfe1a6f6681a8dc77517
Joeypine <fanjiasong_7@outlook.com>
2020-04-14 21:38:06 +0800

    修改语义化版本判断的方法

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,9 +1,5 @@
 package com.github.hcsp.maven;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -18,56 +14,15 @@ public class Version {
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
     public static int compare(String version1, String version2) {
-        //如果是null报异常
-        if (version1 == null || version2 == null) {
-            throw new IllegalArgumentException("argument may not be null !");
-        }
-        //如果相等返回0
-        if (version1.equals(version2)) {
-            return 0;
-        }
+        String[] arr1 = version1.split("\\.");
+        String[] arr2 = version2.split("\\.");
+        int maxLength = Math.max(arr1.length, arr2.length);
+        for (int i = 0; i < maxLength; i++) {
+            int v1 = i < arr1.length ? Integer.parseInt(arr1[i]) : 0;
+            int v2 = i < arr2.length ? Integer.parseInt(arr2[i]) : 0;
 
-        //如果碰到非0-9和点的字符就转为-1，以点为分割放入数组
-        String[] VersionArra = version1.replaceAll("[^0-9.]", "-1").split("[.]");
-        String[] Version2Arb = version2.replaceAll("[^0-9.]", "-1").split("[.]");
-
-        //数组转list，因为Arrays.asList()生产的List没有重写add方法会报错
-        //需要重新放入java.util.ArrayList类
-        List lista = Arrays.asList(VersionArra);
-        List list1 = new ArrayList(lista);
-        List listb = Arrays.asList(Version2Arb);
-        List list2 = new ArrayList(listb);
-
-        //使用List的Add方法补0
-        if (list1.size() == 1) {
-            list1.add("0");
-            list1.add("0");
-        } else if (list1.size() == 2) {
-            list1.add("0");
-        }
-        if (list2.size() == 1) {
-            list2.add("0");
-            list2.add("0");
-        } else if (list2.size() == 2) {
-            list2.add("0");
-        }
-
-        //转回数组格式再比较
-        String[] VersionArr1 = new String[list1.size()];
-        String[] VersionArr2 = new String[list2.size()];
-        for (int i = 0; i < list1.size(); i++) {
-            VersionArr1[i] = (String) list1.get(i);
-        }
-        for (int i = 0; i < list2.size(); i++) {
-            VersionArr2[i] = (String) list2.get(i);
-        }
-
-        //循环比较两个数组中同下标的数值
-        for (int i = 0; i < VersionArr1.length; i++) {
-            if (Integer.parseInt(VersionArr1[i]) < Integer.parseInt(VersionArr2[i])) {
-                return -1;
-            } else if (Integer.parseInt(VersionArr1[i]) > Integer.parseInt(VersionArr2[i])) {
-                return 1;
+            if (v1 != v2) {
+                return v1 > v2 ? 1 : -1;
             }
         }
         return 0;
