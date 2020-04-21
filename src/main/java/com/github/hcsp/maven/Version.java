5b2bc7753135ab329badb2a82773d85fbc09db1b
你的英文名 <>
2020-04-21 17:47:59 +0800

    try 1

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,33 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+
+    public static int compare(String version1, String version2) {
+
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int i = 0;
+        // 比对相应的子串
+        for (; i < v1.length && i < v2.length; i++) {
+            int val1 = Integer.parseInt(v1[i]);
+            int val2 = Integer.parseInt(v2[i]);
+            if (val1 < val2) return -1;
+            if (val1 > val2) return 1;
+        }
+
+        if (v2.length > v1.length) {
+            for (; i < v2.length; i++) {
+                int val = Integer.parseInt(v2[i]);
+                if (val != 0) return -1;
+            }
+        } else if (v1.length > v2.length) {
+            for (; i < v1.length; i++) {
+                int val = Integer.parseInt(v1[i]);
+                if (val != 0) return 1;
+            }
+        }
+        return 0;
+    }
 }
+
+
