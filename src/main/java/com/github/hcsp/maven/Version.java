a16451836194bf4d4a4b4b0f96ae21bc387b5558
你的英文名 <>
2020-04-22 10:14:21 +0800

    try 2

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,33 +13,23 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-
+//这个代码不是我自己写的，是pr仓库里面的。我想了很多办法去改，但改来改去还是觉得这个版本是最好
     public static int compare(String version1, String version2) {
+        String[] arr1 = version1.split("\\.");
+        String[] arr2 = version2.split("\\.");
+        int maxLength = Math.max(arr1.length, arr2.length);
+        for (int i = 0; i < maxLength; i++) {
+            int v1 = i < arr1.length ? Integer.parseInt(arr1[i]) : 0;
+            int v2 = i < arr2.length ? Integer.parseInt(arr2[i]) : 0;
 
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
-        int i = 0;
-        // 比对相应的子串
-        for (; i < v1.length && i < v2.length; i++) {
-            int val1 = Integer.parseInt(v1[i]);
-            int val2 = Integer.parseInt(v2[i]);
-            if (val1 < val2) return -1;
-            if (val1 > val2) return 1;
-        }
-
-        if (v2.length > v1.length) {
-            for (; i < v2.length; i++) {
-                int val = Integer.parseInt(v2[i]);
-                if (val != 0) return -1;
-            }
-        } else if (v1.length > v2.length) {
-            for (; i < v1.length; i++) {
-                int val = Integer.parseInt(v1[i]);
-                if (val != 0) return 1;
+            if (v1 != v2) {
+                return v1 > v2 ? 1 : -1;
             }
         }
         return 0;
     }
+
 }
+
 
 
