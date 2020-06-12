dda36b186666d03214e56bf3bcff1ba7d96afb87
summer <summer_xue.zz@foxmail.com>
2020-06-12 15:11:33 +0800

    自己造轮子实现语义化ç¬版本对æ比

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,6 +1,12 @@
 package com.github.hcsp.maven;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Version {
+
+    private static final int NUM_OF_DIGIT = 3; //可限定版本号位数
+
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
      *
@@ -13,5 +19,52 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    /*
+    1.根据正则表达式按照.进行分割，存到String类型的数组里
+    2.将数组内的值存到ArrayList中
+    3.补全版本号，均变成NUM_OF_DIGIT位
+    4.对这两个list的每一项分别进行比对
+    */
+
+    public static int compare(String version1, String version2) {
+        String[] version1Array = version1.split("[.]");
+        String[] version2Array = version2.split("[.]");
+
+        int len1 = version1Array.length;
+        int len2 = version2Array.length;
+
+        List<String> version1List = new ArrayList<>();
+        List<String> version2List = new ArrayList<>();
+
+        for (int index = 0; index < len1; index++){
+            version1List.add(index, version1Array[index]);
+        }
+        for (int index = 0; index < len2; index++){
+            version2List.add(index, version2Array[index]);
+        }
+
+        if (len1 < NUM_OF_DIGIT) {
+            for (int i = len1; i < NUM_OF_DIGIT; i++) {
+                version1List.add(i, "0");
+            }
+        }
+        if (len2 < NUM_OF_DIGIT) {
+            for (int j = len2; j < NUM_OF_DIGIT; j++) {
+                version2List.add(j, "0");
+            }
+        }
+        if (version1List.equals(version2List)) {
+            return 0;
+        }
+        for (int i = 0; i < NUM_OF_DIGIT; i++) {
+            if (version1List.get(i).equals(version2List.get(i))) {
+                continue;
+            }
+            if (Long.parseLong(version1List.get(i)) > Long.parseLong(version2List.get(i))) {
+                return 1;
+            }
+                return -1;
+        }
+        return 0;
+    }
 }
