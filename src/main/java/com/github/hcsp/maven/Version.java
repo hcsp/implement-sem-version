ae983cdc4cffe7f34287192488593a2bccd96d9b
luckzhu <luckzhu1992@gmail.com>
2020-07-09 17:34:47 +0800

    implement-sem-version (#163)
    implement-sem-version (#163)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -11,7 +11,26 @@ public class Version {
      *
      * @param version1 传入的版本字符串1，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
-     * @return -1/0/1 当version1 小于/等于/大于 version2时
+     * @return int 如果version1小于version2，返回-1；如果version1大于version2，返回1。如果二者相等，返回0。
+     *
      */
-    public static int compare(String version1, String version2) {}
+    // https://stackoverflow.com/questions/198431/how-do-you-compare-two-version-strings-in-java
+    public static int compare(String version1, String version2) {
+        String[] version1Parts = version1.split("\\.");
+        String[] version2Parts = version2.split("\\.");
+        int length = Math.max(version1Parts.length, version2Parts.length);
+        for (int i = 0; i < length; i++) {
+            int version1Part = i < version1Parts.length ?
+                    Integer.parseInt(version1Parts[i]) : 0;
+            int version2Part = i < version2Parts.length ?
+                    Integer.parseInt(version2Parts[i]) : 0;
+            if (version1Part < version2Part) {
+                return -1;
+            }
+            if (version1Part > version2Part) {
+                return 1;
+            }
+        }
+        return 0;
+    }
 }
