bda3bc31b7215d9c8643b1d963f2eb4780409ac6
ZEJIA LIU <53506531+ZEJIA-LIU@users.noreply.github.com>
2020-07-16 01:04:18 +0800

    实现语义化版本 (#165)
    实现语义化版本 (#165)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import java.util.ArrayList;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,37 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        ArrayList<Integer> version1List = toIntArray(version1);
+        ArrayList<Integer> version2List = toIntArray(version2);
+        int maxLength = version1List.size() - version2List.size() > 0 ? version1List.size() : version2List.size();
+        for (int i = 0; i < maxLength; i++) {
+            int version1Num = getVersionNumber(version1List, i);
+            int version2Num = getVersionNumber(version2List, i);
+            if (version1Num > version2Num) {
+                return 1;
+            } else if (version1Num < version2Num) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    private static ArrayList<Integer> toIntArray(String version) {
+        String[] stringArray = version.split("\\.");
+        ArrayList<Integer> intList = new ArrayList<>();
+        for (String s : stringArray) {
+            intList.add(Integer.valueOf(s));
+        }
+        return intList;
+    }
+
+    private static int getVersionNumber(ArrayList<Integer> arrayList, int index) {
+        if (index > arrayList.size() - 1) {
+            return 0;
+        } else {
+            return arrayList.get(index);
+        }
+    }
+
 }
