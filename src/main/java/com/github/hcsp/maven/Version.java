fc8a52f1f06dd45dc5f79e8b384792244f5ca291
pccmast <liu248981@gmail.com>
2020-09-26 10:38:35 +0800

    fix: version compare with maven artifact

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,13 +1,8 @@
 package com.github.hcsp.maven;
 
-import io.github.gravitation1.SemanticVersion;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import org.apache.maven.artifact.versioning.ComparableVersion;
 
 public class Version {
-
-    private static final String SEPARATOR_REGEX = "\\.";
 
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -22,30 +17,9 @@ public class Version {
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
     public static int compare(String version1, String version2) {
-
-        SemanticVersion semanticVersionA = SemanticVersion.from(uniformlength(version1));
-        SemanticVersion semanticVersionB = SemanticVersion.from(uniformlength(version2));
-        return Integer.compare(semanticVersionA.compareTo(semanticVersionB), 0);
-    }
-
-    /**
-     * 将传入的不标准长度的 version 号变成标准的版本格式，以防止 SemanticVersion$InvalidBaseFormatException 报错
-     * @param versionnum 传入的版本号
-     * @return 返回一个 x.y.z 格式的数字
-     */
-    public static String uniformlength(String versionnum) {
-        ArrayList<String> tempver1 = new ArrayList<>(Arrays.asList(versionnum.split(SEPARATOR_REGEX)));
-        int len = tempver1.size();
-
-        for (; len < 3; len++) {
-            tempver1.add("0");
-        }
-
-        String res = tempver1.get(0);
-        for (int i = 0; i < 2; i++) {
-            res += ".";
-            res += tempver1.get(i + 1);
-        }
+        ComparableVersion version1_1 = new ComparableVersion(version1);
+        ComparableVersion version1_2 = new ComparableVersion(version2);
+        int res = version1_1.compareTo(version1_2);
         return res;
     }
 }
