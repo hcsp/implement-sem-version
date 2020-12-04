7b754e200656622b4fe1cd0eb690fdee518989bd
qipengWei <webqipeng@163.com>
2020-12-04 22:07:38 +0800

    参考了大佬写的代码

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,24 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        //用特定字符串转为数组
+        String[] splitVersion1 = version1.split("\\.");
+        String[] splitVersion2 = version2.split("\\.");
+        //返回两者比较最大长度
+        int maxLength = Math.max(splitVersion1.length, splitVersion2.length);
+        //循环两者最大长度
+        for (int i = 0; i < maxLength; i++) {
+            //版本号元素拆分挨个判断 位数没有一律视为0判断
+            int value1 = i < splitVersion1.length ? Integer.parseInt(splitVersion1[i]) : 0;
+            int value2 = i < splitVersion2.length ? Integer.parseInt(splitVersion2[i]) : 0;
+            //拆分后不相等则判断当前位版本号 相等则进入下一轮位数判断
+            if (value1 != value2) {
+                return value1 > value2 ? 1 : -1;
+            }
+        }
+        return 0;
+    }
+
+
 }
