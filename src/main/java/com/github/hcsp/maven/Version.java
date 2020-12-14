eb02ae62efa659fd0c3b7130854ba5b2651d34a7
iRuby <1479867360@qq.com>
2020-12-14 16:44:59 +0800

    比较两个"语义化版本"

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,41 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        return new Ver(version1).compareTo(new Ver(version2));
+    }
+
+    private static class Ver implements Comparable<Ver> {
+        int x;
+        int y;
+        int z;
+
+        public Ver(String str) {
+            //可以假定传入的字符串一定是合法的语义化版本
+            String[] strings = str.split("\\.");
+            x = Integer.parseInt(strings[0]);
+            if (strings.length == 2) {
+                y = Integer.parseInt(strings[1]);
+            }
+            if (strings.length == 3) {
+                y = Integer.parseInt(strings[1]);
+                z = Integer.parseInt(strings[2]);
+            }
+        }
+
+        @Override
+        public int compareTo(Ver ver) {
+            int rex = Integer.compare(this.x, ver.x);
+            if (rex != 0) {
+                return rex;
+            }
+            int rey = Integer.compare(this.y, ver.y);
+            if (rey != 0) {
+                return rey;
+            }
+            return Integer.compare(this.z, ver.z);
+        }
+    }
+
+
 }
