a3529849f65e36567a1b9f8a29b9d439fa8aea22
weiranyi <168578203@qq.com>
2020-12-15 12:58:34 +0800

    借助Integer的compare方法进行等值判断

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,28 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        //创建两个字符串数组
+        /*
+         * 1、字符串中的\\被编译器解释为\
+         * 2、作为正则表达式，\.被正则表达式引擎解释为.
+         */
+        String[] versionOne = version1.split("\\.");
+        String[] versionTwo = version2.split("\\.");
+
+        for (int i = 0; i < Math.max(versionOne.length, versionTwo.length); i++) {
+            Integer versionOneItem = i < versionOne.length ? Integer.parseInt(versionOne[i]) : 0;
+            Integer versionTwoItem = i < versionTwo.length ? Integer.parseInt(versionTwo[i]) : 0;
+            /*
+             *     public static int compare(int x, int y) {
+             *          return (x < y) ? -1 : ((x == y) ? 0 : 1);
+             *     }
+             */
+            if (Integer.compare(versionOneItem, versionTwoItem) != 0) {
+                return Integer.compare(versionOneItem, versionTwoItem);
+            }
+        }
+        return 0;
+    }
+
 }
