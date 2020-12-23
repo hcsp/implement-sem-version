37e13201cd9f813458db88b2f07cfba298c1f82b
XuKaiQuan <964262001@qq.com>
2020-12-23 16:53:11 +0800

    实现语义化版本

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -20,12 +20,16 @@ public class Version {
         String[] version2Arr = version2.split("\\.");
         for (int i = 0; i < MAX_LENGTH; i++) {
             int v1 = version1Arr.length > i ? Integer.parseInt(version1Arr[i]) : 0;
-            int v2 = version2Arr.length > i ? Integer.parseInt(version1Arr[i]) : 0;
+            int v2 = version2Arr.length > i ? Integer.parseInt(version2Arr[i]) : 0;
             if (v1 != v2) {
                 return v1 < v2 ? -1 : 1;
             }
         }
         return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compare("1","1.0.0"));
     }
 
 
