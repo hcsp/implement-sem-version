a9885b9331c8970757865164594b7c4a6aa9ceef
GrumpyCitizenBear <1049378767@qq.com>
2021-10-21 09:47:43 +0800

    实现语义化版本

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.maven;
 
-public class Version {
+import com.github.zafarkhaja.semver.Version;
+
+class VersionCompare {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
      *
@@ -13,5 +15,17 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        com.github.zafarkhaja.semver.Version v1 = com.github.zafarkhaja.semver.Version.valueOf(version1);
+        com.github.zafarkhaja.semver.Version v2 = com.github.zafarkhaja.semver.Version.valueOf(version2);
+
+        if (v1.greaterThan(v2)) {
+            return 1;
+        } else if (v1.lessThan(v2)) {
+            return -1;
+        }
+        return 0;
+
+    }
+
 }
