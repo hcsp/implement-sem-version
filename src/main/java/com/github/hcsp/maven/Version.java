88626bd65fb74c9eecfc6e85bf68d01cc2489ab7
soohea <77014206+soohea@users.noreply.github.com>
2021-10-31 14:57:34 +0800

    implement-sem-version (#210)
    implement-sem-version (#210)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import com.vdurmont.semver4j.Semver;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,34 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+
+
+        Semver sem1 = new Semver(versionCompletion(version1));
+
+        Semver sem2 = new Semver(versionCompletion(version2));
+
+        if (sem1.isGreaterThan(sem2)) {
+            return 1;
+        } else if (sem1.isLowerThan(sem2)) {
+            return -1;
+        }
+        return 0;
+
+    }
+
+    //将version补充完整
+    public static String versionCompletion(String version) {
+        if (version.split("\\.").length == 1) {
+            return (version + ".0.0");
+        } else if (version.split("\\.").length == 2) {
+            return (version + ".0");
+        }
+        return version;
+
+    }
+
+
 }
+
+
