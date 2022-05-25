890b75dcf0a424a1818b94f1db1a92776f2a6c21
GerogeLiu <liujiachi6000@126.com>
2022-05-25 18:44:58 +0800

    update my solution (#223)
    update my solution (#223)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import java.util.Arrays;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,33 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+
+        String[] v1_src = version1.split("\\.");
+        String[] v2_src = version2.split("\\.");
+
+        int len1 = v1_src.length;
+        int len2 = v2_src.length;
+
+        int testTimes = Math.max(len1, len2);
+
+        String[] v1 = new String[testTimes];
+        String[] v2 = new String[testTimes];
+        Arrays.fill(v1, "0");
+        Arrays.fill(v2, "0");
+
+        System.arraycopy(v1_src, 0, v1, 0, len1);
+        System.arraycopy(v2_src, 0, v2, 0, len2);
+
+        for (int i = 0; i < testTimes; i++) {
+
+            if (Integer.parseInt(v1[i]) > Integer.parseInt(v2[i])) {
+                return 1;
+            } else if (Integer.parseInt(v1[i]) < Integer.parseInt(v2[i])){
+                return -1;
+            }
+
+        }
+        return 0;
+    }
 }
