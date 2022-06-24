52589190e0696b03dd4078f2cb863657d5468355
zj <woaiwrf@qq.com>
2022-06-24 23:17:05 +0800

    version

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,25 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        String[] versionArr1 = version1.split("\\.");
+        String[] versionArr2 = version2.split("\\.");
+        int ver1 = 0;
+        int ver2 = 0;
+        for (int i = 0; i < 3; i++) {
+            if (i < versionArr1.length) {
+                ver1 = ver1 * 10 + Integer.valueOf(versionArr1[i]);
+            } else {
+                ver1 = ver1 * 10;
+            }
+            if (i < versionArr2.length) {
+                ver2 = ver2 * 10 + Integer.valueOf(versionArr2[i]);
+            } else {
+                ver2 = ver2 * 10;
+            }
+        }
+        return Integer.compare(ver1, ver2);
+    }
+
+
 }
