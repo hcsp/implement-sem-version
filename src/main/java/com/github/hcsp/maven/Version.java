d82b41daa4ec8c3a1df94c6e2f55d0b443673ebe
nekoha_rf <39894049+rf818@users.noreply.github.com>
2023-11-27 01:23:17 +0800

    implement-sem-version (#237)
    implement-sem-version (#237)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,6 +1,12 @@
 package com.github.hcsp.maven;
 
 public class Version {
+
+
+    public static void main(String[] args) {
+        compare("1.0", "1");
+    }
+
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
      *
@@ -13,5 +19,19 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    private static final int MAX_NUM = 3;
+
+    public static int compare(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        for (int i = 0; i < MAX_NUM; i++) {
+            int value1 = (i < v1.length) ? Integer.parseInt(v1[i]) : 0;
+            int value2 = (i < v2.length) ? Integer.parseInt(v2[i]) : 0;
+            if (value1 != value2) {
+                return (value1 < value2) ? -1 : 1;
+            }
+        }
+        return 0;
+    }
+
 }
