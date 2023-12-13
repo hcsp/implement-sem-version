a54994f632530cd902aa63100099c1d2afd8bde3
magicalmuggle <magicalmuggle@foxmail.com>
2023-12-13 22:36:41 +0800

    complete å¦method

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,46 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        int[] v1 = splitVersion(version1);
+        int[] v2 = splitVersion(version2);
+
+        return compareVersion(v1, v2);
+    }
+
+    private static int[] splitVersion(String version) {
+        String[] parts = version.split("\\.");
+        int[] versionArray = new int[parts.length];
+
+        for (int i = 0; i < parts.length; i++) {
+            versionArray[i] = Integer.parseInt(parts[i]);
+        }
+
+        return versionArray;
+    }
+
+    private static int compareVersion(int[] v1, int[] v2) {
+        int maxLength = Math.max(v1.length, v2.length);
+
+        for (int i = 0; i < maxLength; i++) {
+            int v1Number = (i < v1.length) ? v1[i] : 0;
+            int v2Number = (i < v2.length) ? v2[i] : 0;
+
+            if (v1Number < v2Number) {
+                return -1;
+            } else if (v1Number > v2Number) {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // 测试
+        System.out.println(compare("1.0.0", "1.0.0"));
+        System.out.println(compare("1.0.0", "1.0.1"));
+        System.out.println(compare("1.0.0", "1.1.0"));
+        System.out.println(compare("1.0.0", "2.0.0"));
+    }
 }
