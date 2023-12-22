e3b6d1cbf65112fef993595011183a20744f0bb8
Leo Cheung <leozhang621@gmail.com>
2023-12-22 19:25:54 +0800

    completed missions (#240)
    completed missions (#240)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,6 @@
 package com.github.hcsp.maven;
 
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +14,18 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        String[] version1Parts = version1.split("\\.");
+        String[] version2Parts = version2.split("\\.");
+        int length = Math.max(version1Parts.length, version2Parts.length);
+        for (int i = 0; i < length; i++) {
+            int part1 = i >= version1Parts.length ? 0 : Integer.parseInt(version1Parts[i]);
+            int part2 = i >= version2Parts.length ? 0 : Integer.parseInt(version2Parts[i]);
+            if (part1 == part2) {
+                continue;
+            }
+            return part1 - part2;
+        }
+        return 0;
+    }
 }
